cc804469de025ed2f5174f93d972da350a9d3e61
HowardLei <410905481@qq.com>
2020-11-26 00:21:28 +0800

    更新配置

--- src/main/java/com/itcenter/chat/ChatApplication.java
+++ src/main/java/com/itcenter/chat/ChatApplication.java
@@ -1,26 +1,17 @@
 package com.itcenter.chat;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author HowardLei
  */
 @SpringBootApplication
 @EnableWebSocket
-@Slf4j
 public class ChatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
-    }
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
     }
 }
